1eacd7a4e3a36c0b5ef9fc52a5b5d18a5709f636
deathslayer5-dev <deathslayer532@gmail.com>
2026-03-26 09:59:15 -0400

    website ver 1, img place holders

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// script.js
+function openPopup() {
+    const messages = [
+        "Growth happens through action.",
+        "Kindness shapes your world.",
+        "Every emotion has meaning.",
+        "You are learning, always."
+    ];
+
+    document.getElementById("popup-text").textContent = messages[Math.floor(Math.random()*messages.length)];
+    document.getElementById("popup").classList.remove("hidden");
+}
+
+function closePopup() {
+    document.getElementById("popup").classList.add("hidden");
+}
